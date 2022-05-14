8c5f64d0a5cc19322b76c5be7282232bcfe8241e
ChikleNeha <chikleneha473@gmail.com>
2022-05-14 20:22:36 +0530

    3 files modified, 1 added, 1 deleted

---
+++ src/Div.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Div(props) {
+
+  return (
+    <div className = "the--divs">
+       <p>{props.item.p1}</p>
+    </div>
+  );
+}
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Nav from './Nav'
-import CoursesBar from './CoursesBar'
 import data from './data'
+import Div from './Div'
 
 export default function Home() {
 
   const newData = data.map( item => {
     return (
-      <CoursesBar 
-         item ={item}
+      <Div 
+      item = {item}
       />
     )
   })
@@ -18,9 +18,9 @@ export default function Home() {
         <div className="home">
             <Nav />
             <div className="home--body">
-                <img src="https://img.freepik.com/free-vector/online-learning-isometric-concept_1284-17947.jpg?t=st=1652269943~exp=1652270543~hmac=6f11da7f46beb6efc992f56f7864c9357437b47510f2c84b31beef666261832a&w=740" width="650px"/>
+                <img src="https://img.freepik.com/free-vector/online-learning-isometric-concept_1284-17947.jpg?t=st=1652269943~exp=1652270543~hmac=6f11da7f46beb6efc992f56f7864c9357437b47510f2c84b31beef666261832a&w=740" width="690px"/>
                 <div  className="image--text1">
-                    <div>
+                    <div className="investigating--text--block">
                         <h1>Investing in</h1> 
                         <h1>Knowledge and</h1> 
                         <h1>Your future</h1>
@@ -30,8 +30,8 @@ export default function Home() {
                 </div>
             </div>
             <h4>Browse top essential career courses</h4>
+            <Div />
         </div>
-      {newData}
     </div>
 
     
