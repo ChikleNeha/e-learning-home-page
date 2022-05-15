2087745be7a0658674d3b72f8991e2b77fbe9dce
ChikleNeha <chikleneha473@gmail.com>
2022-05-15 15:27:51 +0530

    3 files modified

--- src/Courses.js
+++ src/Courses.js
@@ -4,10 +4,10 @@ export default function Courses(props) {
   return (
     <div className="courses">
       <div className="the--image">
-        <img src={props.item.Image} width="340px"/>
+        <img src={props.item.Image} width="340px" id="the--course--image"/>
       </div>
-      {props.item.name}
-      {props.item.description}
+      <h1>{props.item.name}</h1>
+      <p>{props.item.description}</p>
     </div>
   )
 }
--- src/coursesData.js
+++ src/coursesData.js
@@ -1,19 +1,19 @@
 export default [
   {
     id : 1,
-    Image : "https://img.freepik.com/free-vector/html5-programming-internet-website-development-web-application-engineering-script-writing-html-code-optimization-programmer-fixing-bugs_335657-2678.jpg?t=st=1652604371~exp=1652604971~hmac=dfc177fe468cc424e100d1dc006f4ac83158056035bd31e711b1ba9631c01caa&w=740",
+    Image : "https://techvccloud.mediacdn.vn/280518386289090560/2021/11/19/html-la-gi-1637311806719307661894-0-20-314-579-crop-16373118117691819782791.jpg",
     name : "HTML",
     description : "Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML",
   },
   {
     id : 2,
-    Image : "https://img.freepik.com/free-vector/programmer-working-with-c_52683-23278.jpg?t=st=1652604371~exp=1652604971~hmac=6c16d38ec5d683942581aa34f4256a685d2b6f516cb2974ac4815303ea6cf74a&w=740",
+    Image : "https://res.cloudinary.com/practicaldev/image/fetch/s--cqZIl0gD--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://thepracticaldev.s3.amazonaws.com/i/elytski1o23ybosxmors.jpg",
     name : "CSS",
     description : "Learn CSS online at your own pace. Start today and improve your skills.",
   },
   {
     id : 3,
-    Image : "https://img.freepik.com/free-vector/programmers-using-javascript-programming-language-computer-tiny-people-javascript-language-javascript-engine-js-web-development-concept-bright-vibrant-violet-isolated-illustration_335657-986.jpg?t=st=1652604610~exp=1652605210~hmac=049ab4520358deed2daa9bebe853a8f4a4d1956b389f51acf369a58f9ee46546&w=740",
+    Image : "https://miro.medium.com/max/1400/1*RR6spBZYaiATXhipWdvNlg.jpeg",
     name : "JAVASCRIPT",
     description : "Learn JavaScript – Free JS Courses for Beginners ; JavaScript Algorithms and Data Structures",
   },
@@ -31,7 +31,7 @@ export default [
   },
   {
     id : 6,
-    Image : "https://img.freepik.com/free-vector/site-stats-concept-illustration_114360-1434.jpg?t=st=1652605442~exp=1652606042~hmac=22d7804adc4b93385ba9293f0fa589d9e647455c6a11368c163fd2932118872f&w=740",
+    Image : "http://www.mycslab.in/wp-content/uploads/2020/01/1214005f6e5f8cf0b48bc88f70dc3e7a-rimg-w2560-h1440-gmir-1024x576.jpg",
     name : "DATA STRUCTURES & ALGORITHMS",
     description : "The DSA online course is designed to improve your problem-solving and coding skills by enhancing your understanding of Data Structures & Algorithms.",
   },
