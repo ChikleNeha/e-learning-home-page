28867d0f6bad56205aea4e2577f29e3225fb5a73
ChikleNeha <chikleneha473@gmail.com>
2022-05-15 17:58:48 +0530

    3 files modified

--- src/Footer.js
+++ src/Footer.js
@@ -3,22 +3,26 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer">
-      <div>
-        <h1>About Us</h1>
-        <p>Teacher</p>
-        <p>Gallery</p>
+      <div className="footer--inside--div">
+            <h1>E-Online.</h1>
+            <div>
+                  <h1>About Us</h1>
+                  <p>Teacher</p>
+                  <p>Gallery</p>
+            </div>
+            <div>
+                  <h1>Service</h1>
+                  <p>Learning</p>
+                  <p>Contact</p>
+                  <p>Class</p>
+            </div>
+            <div>
+                <h1>Further Information</h1>
+                <p>Terms & Condition</p>
+                <p>Privacy Policy</p>
+            </div>
+        </div>
       </div>
-      <div>
-        <h1>Service</h1>
-        <p>Learning</p>
-        <p>Contact</p>
-        <p>Class</p>
-      </div>
-      <div>
-      <h1>Further Information</h1>
-      <p>Terms & Condition</p>
-      <p>Privacy Policy</p>
-      </div>
-    </div>
+    
   )
 }
--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Nav() {
   return (
     <div className="home--page">
-    <h1 id="logo">E-Online</h1>
+    <h1 id="logo">E-Online.</h1>
     <div className="header">
         <a className="a--tag" href="/">Home</a>
         <a className="a--tag"  href='/'>Category</a>
